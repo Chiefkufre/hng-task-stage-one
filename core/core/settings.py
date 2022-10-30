17137a4dcc9a57ff320b7f74787fee2623aec4f5
chiefkufre <samuelkufrewillie@gmail.com>
2022-10-30 13:02:56 +0100

    chore: handled allowed host error

--- core/core/settings.py
+++ core/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = random.randbytes(200)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
