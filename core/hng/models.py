93630811f92f7bafa1b2860c37720afc9c7db339
chiefkufre <samuelkufrewillie@gmail.com>
2022-10-31 04:15:10 +0100

    chore: add task model

--- core/hng/models.py
+++ core/hng/models.py
@@ -14,3 +14,16 @@ class HngUserModel(models.Model):
 
     def nickname(self):
         return f"{self.first_name}" + random.randint(3, 10)
+
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        HngUserModel, 
+        on_delete=models.SET_NULL,
+        null = True,
+        blank = True,
+    )
+    
+    date_created= models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    task_id = models.IntegerField(primary_key=True)
