cfe90acf3fa7f379c97540bc2541855963b14708
chiefkufre <samuelkufrewillie@gmail.com>
2022-11-05 18:19:37 +0100

    chore: build out openai response

---
+++ core/hng/pytest.py
@@ -0,0 +1,11 @@
+
+import re
+
+string = "I am in love"
+
+# n = [i for i in string]
+
+n = string.split()
+
+print(n)
+
--- core/hng/views.py
+++ core/hng/views.py
@@ -4,8 +4,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 import os
-# import openai
+import openai
 import json
+
+from .utilis import openai_response
 
 def user_detail(response, *args, **kwargs):
     
@@ -36,44 +38,74 @@ def user_detail(response, *args, **kwargs):
 
 
 
+openai.api_key = os.environ.get("OPENAI_APP_KEY ")
 
 @csrf_exempt
-def perform_calculation(request, operation_type=None, output=None):
+def perform_calculation(request:HttpRequest, operation_type=None, output=None):
 
     operations = {"addition": "+", "subtraction": "-", "multiplication": "*",}
-
+    
     if request.POST:
         data = request.POST
     elif request.GET:
-        return HttpResponse({"Only POST requests allowed in this endpoint"})
+        return JsonResponse(data={"Only POST requests allowed in this endpoint"})
+    
+    elif request.body:
+
+        data = json.loads(request.body)
+    
+        # convert json input to integers
+        first_value = int(data['y'])
+        second_value = int(data['x'])
+
+        ops = data["operation_type"]
+
+        # pre-processing-convert to lower case-remove white spaces-
+
+        operation = ops.lower().strip().replace(" ", "")
+
+        # check if operation is in operations dictionary
+
+        if operation in operations:
+            operation_type = operation
+
+            # convert out put to integer
+            output = int(eval(f"{first_value}{operations[operation_type]}{second_value}"))
+            context = {
+                 "slackUsername": "Chiefkufre",
+                "result": output,
+                "operation_type": operation_type
+            }
+               
+            return JsonResponse(context)
     
     else:
         data = json.loads(request.body)
+        query = str(data['prompt'])
+
+        response = openai.Completion.create(
+            model = "text-davinci-002",
+            prompt = query,
+            temperature=0,
+            max_tokens=130,
+            frequency_penalty=0,
+            presence_penalty=0
+
+        )
+        if "choices" in response:
+            alternative_terms = {"sum":"addition", "multiplication":"product", "differnce":"subtraction"}
+            output = response['choices'][0]['text']
+            for ops in alternative_terms:
+                operation_type = None
+                new_query = set(query.split())
+                if ops in new_query:
+                    operation_type = ops.lower()
+            return JsonResponse({
+                "slackUsername": "Chiefkufre",
+                "result": output,
+                "operation_type": operation_type
+            })
+        # operation_type = data['operation_type']
+        # output = "Invalid operation_type: {0}.Please enter a valid operational type-like adittion | multiplication | substraction".format(operation_type)
+
     
-
-    first_value = int(data['y'])
-    second_value = int(data['x'])
-
-    ops = data["operation_type"]
-
-    # pre-processing-convert to lower case-remove white spaces-
-
-    operation = ops.lower().strip().replace(" ", "")
-
-    # check if operation ia in operations dictionary
-
-    if operation in operations:
-        operation_type = operation
-        output = int(eval(f"{first_value}{operations[operation_type]}{second_value}"))
-    
-    else:
-        operation_type = data['operation_type']
-        output = "Invalid operation_type: {0}.Please enter a valid operational type-like adittion | multiplication | substraction".format(operation_type)
-
-    context = {
-        "slackUsername": "Chiefkufre",
-        "result": output,
-        "operation_type": operation_type
-    }
-    
-    return JsonResponse(context)
